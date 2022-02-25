6a35723a9f096786868dc1cf27aef6cdf466e5c6
Jiang Yuxin <jyx11011@gmail.com>
2022-02-25 14:11:21 +0800

    modify

--- dreamer/agents/dreamer_agent.py
+++ dreamer/agents/dreamer_agent.py
@@ -43,7 +43,8 @@ class DreamerAgent(RecurrentAgentMixin, BaseAgent):
         """
         model_inputs = buffer_to((observation, prev_action), device=self.device)
         action, state = self.model(*model_inputs, self.prev_rnn_state, self._itr<=5000)
-        action = self.exploration(action)
+        if self._itr>5000:
+            action = self.exploration(action)
         # Model handles None, but Buffer does not, make zeros if needed:
         prev_state = self.prev_rnn_state or buffer_func(state, torch.zeros_like)
         self.advance_rnn_state(state)
