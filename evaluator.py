1454fe71f746e9494ec42203163b568a4353ca7a
kaiqi <jyx11011@gmail.com>
2022-02-26 16:29:00 +0800

    modify

--- evaluator.py
+++ evaluator.py
@@ -5,7 +5,7 @@ from rlpyt.utils.buffer import numpify_buffer, torchify_buffer
 from rlpyt.utils.logging import logger
 
 class Evaluator:
-    def __init__(self, agent, env, T=100):
+    def __init__(self, agent, env, T=50):
         self.env = env
         self.agent = agent
         self.T = T
@@ -16,15 +16,20 @@ class Evaluator:
         self.agent.eval_mode(itr)
 
         observation = torchify_buffer(self.env.reset())
-
+        action = torch.zeros(1, 1, device=self.agent.device)
+        reward = None
+        
         for t in range(self.T):
-            act, _ = self.agent.step(observation, None, None)
-            action = numpify_buffer(act_pyt)
+            observation = observation.unsqueeze(0)
+            action, _ = self.agent.step(observation, action, reward)
+            act = numpify_buffer(action)[0] 
             obs, r, d, env_info = self.env.step(action)
 
             if d:
                 logger.log("Done in " f"{t} steps.")
                 break
+
+            observation = torch.tensor(obs)
         
         logger.log("position: "f"{self.env._env.physics.bounded_position()} and velocity: "f"{self.env._env.physics.velocity()}.")
 
