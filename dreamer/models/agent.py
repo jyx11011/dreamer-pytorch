ed36d6b9b6c597346e52744a8ae018cf688e1640
kaiqi <jyx11011@gmail.com>
2022-02-28 00:11:29 +0800

    low dim

--- dreamer/models/agent.py
+++ dreamer/models/agent.py
@@ -16,15 +16,15 @@ class AgentModel(nn.Module):
     def __init__(
             self,
             action_shape,
-            stochastic_size=30,
-            deterministic_size=200,
-            hidden_size=200,
+            stochastic_size=3,
+            deterministic_size=10,
+            hidden_size=10,
             image_shape=(3, 64, 64),
             action_dist='one_hot',
             dtype=torch.float,
             use_pcont=False,
-            pcont_layers=30,
-            pcont_hidden=200,
+            pcont_layers=3,
+            pcont_hidden=10,
             **kwargs,
     ):
         super().__init__()
