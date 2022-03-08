52d5780f7d55008927ee498bd22ab0aec4bf357d
kaiqi-ken <jyx11011@gmail.com>
2022-03-08 16:39:03 +0700

    modify

--- dreamer/models/mpc_planner.py
+++ dreamer/models/mpc_planner.py
@@ -24,7 +24,7 @@ class Dynamics(torch.nn.Module):
 
 class MPC_planner:
     def __init__(self, nx, nu, dynamics,
-            timesteps=10,
+            timesteps=20,
             goal_weights=None, ctrl_penalty=0.001, iter=50,
             action_low=-1.0, action_high=1.0):
         self._timesteps=timesteps
@@ -72,13 +72,13 @@ class MPC_planner:
                         lqr_iter=self._iter, 
                         n_batch=n_batch,
                         u_init=self._u_init,
-                        max_linesearch_iter=20,
-                        linesearch_decay=0.3,
+                        max_linesearch_iter=10,
+                        linesearch_decay=0.2,
                         exit_unconverged=False, 
                         detach_unconverged = True, 
-                        verbose=1,
+                        verbose=0,
                         eps=1e-2,
-			delta_u=0.01,
+			#delta_u=0.1,
                         grad_method=mpc.GradMethods.AUTO_DIFF)
             nominal_states, nominal_actions, nominal_objs = ctrl(state, self._cost, self._dynamics)
         action = nominal_actions[:num]
