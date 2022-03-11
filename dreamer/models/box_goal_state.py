ae55c522993067ab6747eb816a010008b5b1eca1
Jiang Yuxin <jyx11011@gmail.com>
2022-03-12 00:38:48 +0800

    modify

--- dreamer/models/box_goal_state.py
+++ dreamer/models/box_goal_state.py
@@ -1,3 +1,6 @@
+import argparse
+import numpy as np
+
 from dm_control import mujoco
 from dm_control.rl import control
 from dm_control.suite import base
@@ -8,11 +11,17 @@ from lxml import etree
 from dreamer.envs.box import Physics
 
 
-def goal_obs():
+def goal_obs(bx=0):
     f=open("dreamer/envs/box.xml","r")
-    MODEL_XML = f.read()
+    MODEL_XML = f.read().format(bx=bx)
     physics = Physics.from_xml_string(MODEL_XML, common.ASSETS)
-    with self.physics.reset_context(): 
-        physics.named.data.qpos['slider'] = 2
     print("goal:", physics.box_position())
     return physics.render(64, 64, camera_id=0).transpose(2, 0, 1).copy()
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--bx', help='box position', default=0)
+    obs = goal_obs(args.bx)
+    f='box_goal_state_'+str(bx)
+    np.save(f,obs)
