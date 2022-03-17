34ca1474f0930ee9bd4752309c302a99990b92fa
kaiqi-ken <jyx11011@gmail.com>
2022-03-17 17:47:23 +0700

    modify

--- dreamer/models/mpc_planner.py
+++ dreamer/models/mpc_planner.py
@@ -25,7 +25,7 @@ class Dynamics(torch.nn.Module):
 
 class MPC_planner:
     def __init__(self, nx, nu, dynamics,
-            timesteps=100,
+            timesteps=20,
             goal_weights=None, ctrl_penalty=0.001, iter=50,
             action_low=-1.0, action_high=1.0):
         self._timesteps=timesteps
@@ -66,8 +66,8 @@ class MPC_planner:
         if num > self._timesteps:
             num = self._timesteps
         n_batch = state.shape[0]
-        if self._u_init is None:
-            self._u_init = torch.clamp(torch.randn(self._timesteps, n_batch, self._nu),-1,1)
+        #if self._u_init is None:
+        self._u_init = torch.rand(self._timesteps, n_batch, self._nu)*2-1
         state = torch.clone(state)
 
         with torch.enable_grad():
@@ -83,7 +83,7 @@ class MPC_planner:
                         #detach_unconverged = False, 
                         verbose=1,
                         backprop=False,
-                        eps=1e-2,
+                        eps=1e-5,
                         #delta_u=0.5,
                         grad_method=mpc.GradMethods.AUTO_DIFF)
             nominal_states, nominal_actions, nominal_objs = ctrl(state, self._cost, self._dynamics)
