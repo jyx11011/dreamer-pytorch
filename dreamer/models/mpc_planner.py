b6377189c00856ad065ceee665ef00e01f87c5e1
kaiqi-ken <jyx11011@gmail.com>
2022-03-17 17:29:27 +0700

    modify

--- dreamer/models/mpc_planner.py
+++ dreamer/models/mpc_planner.py
@@ -66,8 +66,8 @@ class MPC_planner:
         if num > self._timesteps:
             num = self._timesteps
         n_batch = state.shape[0]
-        if self._u_init is None:
-            self._u_init=torch.rand(self._timesteps, n_batch, self._nu)*2-1
+        #if self._u_init is None:
+        self._u_init=torch.rand(self._timesteps, n_batch, self._nu)*2-1
         state = torch.clone(state)
         with torch.enable_grad():
             ctrl = mpc.MPC(self._nx, self._nu, self._timesteps, 
@@ -87,8 +87,8 @@ class MPC_planner:
                         grad_method=mpc.GradMethods.AUTO_DIFF)
             nominal_states, nominal_actions, nominal_objs = ctrl(state, self._cost, self._dynamics)
         action = nominal_actions[:num]
-        if mode == 'eval':
-            self._u_init = torch.cat((nominal_actions[num:], torch.rand(num, n_batch, self._nu, dtype=self._dtype,device=action.device) * 2 - 1), dim=0)
+        #if mode == 'eval':
+        #    self._u_init = torch.cat((nominal_actions[num:], torch.rand(num, n_batch, self._nu, dtype=self._dtype,device=action.device) * 2 - 1), dim=0)
         return action
 
 def load_goal_state(dtype):
