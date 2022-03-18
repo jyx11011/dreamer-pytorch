d46783ad44b6388e5fc94b464f34b4c251e1b80d
kaiqi-ken <jyx11011@gmail.com>
2022-03-18 13:22:21 +0700

    modify

--- dreamer/models/mpc_planner.py
+++ dreamer/models/mpc_planner.py
@@ -25,7 +25,7 @@ class Dynamics(torch.nn.Module):
 
 class MPC_planner:
     def __init__(self, nx, nu, dynamics,
-            timesteps=100,
+            timesteps=50,
             goal_weights=None, ctrl_penalty=0.001, iter=50,
             action_low=-1.0, action_high=1.0):
         self._timesteps=timesteps
@@ -38,7 +38,7 @@ class MPC_planner:
         self._dtype=torch.float
 
         if goal_weights is None:
-            goal_weights = 20*torch.ones(nx, dtype=self._dtype)
+            goal_weights = 10*torch.ones(nx, dtype=self._dtype)
         self._goal_weights = goal_weights
         q = torch.cat((
             goal_weights,
@@ -65,7 +65,7 @@ class MPC_planner:
             num = self._timesteps
         n_batch = state.shape[0]
         if self._u_init is None:
-            self._u_init = torch.rand(self._timesteps, n_batch, self._nu) * 2 - 1#torch.clamp(torch.randn(self._timesteps, n_batch, self._nu),-1,1)
+            self._u_init = torch.rand(self._timesteps, n_batch, self._nu,device=state.device) * 2 - 1#torch.clamp(torch.randn(self._timesteps, n_batch, self._nu),-1,1)
         state = torch.clone(state)
 
         with torch.enable_grad():
@@ -76,12 +76,12 @@ class MPC_planner:
                         n_batch=n_batch,
                         u_init=self._u_init,
                         max_linesearch_iter=20,
-                        linesearch_decay=0.2,
+                        linesearch_decay=0.5,
                         exit_unconverged=False, 
                         backprop=False,
                         #detach_unconverged = False, 
                         verbose=1,
-                        eps=1e-2,
+                        eps=1e-10,
                         #delta_u=0.5,
                         grad_method=mpc.GradMethods.AUTO_DIFF)
             nominal_states, nominal_actions, nominal_objs = ctrl(state, self._cost, self._dynamics)
