4ef5bdf97732de76f534c9f1c1556ac25df18e23
kaiqi-ken <jyx11011@gmail.com>
2022-03-20 10:46:39 +0700

    modify mpc planner

--- dreamer/models/mpc_planner.py
+++ dreamer/models/mpc_planner.py
@@ -86,7 +86,7 @@ class MPC_planner:
                         eps=configs.eps,
 		                delta_u=configs.delta_u,
                         grad_method=mpc.GradMethods.AUTO_DIFF)
-            nominal_states, nominal_actions, nominal_objs = ctrl(state, self._cost, self.dynamics)
+            nominal_states, nominal_actions, nominal_objs = ctrl(state, self._cost, self._dynamics)
         action = nominal_actions[:num]
         if mode == 'eval':
             self._u_init = torch.cat((nominal_actions[num:], torch.rand(num, n_batch, self._nu, dtype=self._dtype,device=action.device) * 2 - 1), dim=0)
