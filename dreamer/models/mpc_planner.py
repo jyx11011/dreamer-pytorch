ef4cb38e54ba7c75e7a1520aa6f735fb961d32c7
kaiqi-ken <jyx11011@gmail.com>
2022-03-23 12:11:17 +0700

    modify

--- dreamer/models/mpc_planner.py
+++ dreamer/models/mpc_planner.py
@@ -67,7 +67,7 @@ class MPC_planner:
             num = self._timesteps
         n_batch = state.shape[0]
         #if self._u_init is None:
-        self._u_init=torch.rand(self._timesteps, n_batch, self._nu)*2-1
+        #self._u_init=torch.rand(self._timesteps, n_batch, self._nu)*2-1
         state = torch.clone(state)
 
         with torch.enable_grad():
@@ -82,14 +82,14 @@ class MPC_planner:
                         exit_unconverged=False, 
                         detach_unconverged = configs.detach_unconverged, 
                         backprop=configs.backprop,
-                        verbose=1,
+                        verbose=0,
                         eps=configs.eps,
 		        delta_u=configs.delta_u,
                         grad_method=mpc.GradMethods.AUTO_DIFF)
             nominal_states, nominal_actions, nominal_objs = ctrl(state, self._cost, self._dynamics)
         action = nominal_actions[:num]
-        #if mode == 'eval':
-        #    self._u_init = torch.cat((nominal_actions[num:], torch.rand(num, n_batch, self._nu, dtype=self._dtype,device=action.device) * 2 - 1), dim=0)
+        if mode == 'eval':
+            self._u_init = torch.cat((nominal_actions[num:], torch.zeros(num, n_batch, self._nu, dtype=self._dtype,device=action.device)), dim=0)
         return action
 
 def load_goal_state(dtype, domain = "cartpole", task = "balance"):
