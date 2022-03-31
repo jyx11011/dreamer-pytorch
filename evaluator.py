0de5cb8e2a606b8add62f07c2832889178540d0d
Jiang Yuxin <jyx11011@gmail.com>
2022-04-01 03:57:23 +0800

    modify

--- evaluator.py
+++ evaluator.py
@@ -70,7 +70,7 @@ class Evaluator:
         logger.log("position: "f"{self.env.get_obs()}, reward: "f"{tot}")
 
 
-    def eval_model(self, T=20,rand=True,save=10):
+    def eval_model(self, T=20,rand=True,save=10,t=5):
         model = self.agent.model
         self.agent.reset()
         self.agent.eval_mode(0)
@@ -106,15 +106,17 @@ class Evaluator:
         with torch.no_grad():
             embed = model.observation_encoder(observations)
             prev_state = model.get_state_representation(observations[0])
-            prior = model.rollout.rollout_transition(T-1, actions[:-1], prev_state)
-            feat = get_feat(prior)
-            image_pred = torch.cat((observations[0][0].unsqueeze(0).unsqueeze(1)
-                         ,model.observation_decoder(feat).mean))
+            
             prev_state=model.representation.initial_state(1, device=device, dtype=torch.float)
             _, post = model.rollout.rollout_representation(T, embed, actions, prev_state)
 
             feat = get_feat(post)
             post_pred = model.observation_decoder(feat).mean
+
+            prior = model.rollout.rollout_transition(T-t, actions[t-1:-1], prev_state)
+            feat = get_feat(prior)
+            image_pred = torch.cat((post_pred[:t]
+                         ,model.observation_decoder(feat).mean))
         diff=torch.abs(observations[:]-image_pred)
         img_p=np.clip((np.array(image_pred)+0.5)*255,0,255).squeeze(1).transpose((0,2,3,1)).astype(np.uint8)
         img_post=np.clip((np.array(post_pred)+0.5)*255,0,255).squeeze(1).transpose((0,2,3,1)).astype(np.uint8)
@@ -133,7 +135,7 @@ class Evaluator:
         '''
 
 def eval(load_model_path, cuda_idx=None, game="cartpole_balance",itr=10, eval_model=None, 
-        save=True, log_dir=None,rand=True,T=100, min_cos=0.98):
+        save=True, log_dir=None,rand=True,T=100, min_cos=0.98,t=5):
     domain, task = game.split('_')
     
     domain, task = game.split('_',1)
@@ -158,7 +160,7 @@ def eval(load_model_path, cuda_idx=None, game="cartpole_balance",itr=10, eval_mo
     evaluator=Evaluator(agent, env, cuda_idx=cuda_idx,game=game,T=T,min_cos=min_cos)
     
     if eval_model is not None:
-        evaluator.eval_model(T=eval_model,rand=rand)
+        evaluator.eval_model(T=eval_model,rand=rand,t=t)
     else:
         for i in tqdm(range(itr)):
             path = None
@@ -184,6 +186,8 @@ if __name__ == "__main__":
     parser.add_argument('--run-ID', help='run identifier (logging)', type=int, default=0)
     parser.add_argument('--load-model-path', help='load model from path', type=str)  # path to params.pkl
     parser.add_argument('--model', help='evaluate model', type=int, default=None)
+    parser.add_argument('--t', help='evaluate model', type=int, default=5)
+    
     parser.add_argument('--itr', help='total iter', type=int,default=10)  # path to params.pkl
 
     parser.add_argument('--rand', help='rand action', type=bool,default=True)  # path to params.pkl
@@ -216,6 +220,7 @@ if __name__ == "__main__":
         game=args.game,
         itr=args.itr,
         eval_model=args.model,
+        t=args.t,
         save=args.save,
         log_dir=log_dir,
         T=args.T,
