b383fb00b00ae767e32d5b2817d05b2465fa44fb
Jiang Yuxin <jyx11011@gmail.com>
2022-04-01 12:50:56 +0800

    modify evaluator

--- evaluator.py
+++ evaluator.py
@@ -21,12 +21,14 @@ from rlpyt.utils.buffer import numpify_buffer, torchify_buffer
 from rlpyt.utils.logging import logger
 
 class Evaluator:
-    def __init__(self, agent, env, T=100, cuda_idx=None, game='cartpole_balance'):
+    def __init__(self, agent, env, T=100, cuda_idx=None, game='cartpole_balance', min_cos=0.98):
         self.env = env
         self.agent = agent
         self.T = T
         self.cuda_idx = cuda_idx
         self.game = game
+        self.action_dim=env.spaces.action.shape[0]
+        self.min_cos=min_cos
 
     def ctrl(self, itr, verbose=False, log_path=None):
         logger.log("\nStart evaluating: "f"{itr}")
@@ -36,7 +38,7 @@ class Evaluator:
         device = torch.device("cuda:" + str(self.cuda_idx)) if self.cuda_idx is not None else torch.device("cpu")
 
         observation = torchify_buffer(self.env.reset()).type(torch.float)
-        action = torch.zeros(1, 1, device=self.agent.device).to(device)
+        action = torch.zeros(1, self.action_dim, device=self.agent.device).to(device)
         reward = None
 
         observations = []
@@ -61,14 +63,14 @@ class Evaluator:
             observation = torch.tensor(obs).type(torch.float)
 
             if self.game == 'cartpole_balance':
-                if np.abs(self.env.get_obs()['position'][1]) <= 0.98:
+                if np.abs(self.env.get_obs()['position'][1]) <= self.min_cos:
                     break
         if log_path is not None:
             np.savez(log_path, observations=observations, actions=actions)
         logger.log("position: "f"{self.env.get_obs()}, reward: "f"{tot}")
 
 
-    def eval_model(self, T=20,rand=True,save=10):
+    def eval_model(self, T=20,rand=True,save=10,t=5):
         model = self.agent.model
         self.agent.reset()
         self.agent.eval_mode(0)
@@ -79,7 +81,7 @@ class Evaluator:
 
         observation = torchify_buffer(self.env.reset()).type(torch.float)
         observations = [observation]
-        action = torch.zeros(1, 1, device=self.agent.device).to(device)
+        action = torch.zeros(1, self.action_dim, device=self.agent.device).to(device)
         reward = None
         actions = []
         tot=0
@@ -104,15 +106,17 @@ class Evaluator:
         with torch.no_grad():
             embed = model.observation_encoder(observations)
             prev_state = model.get_state_representation(observations[0])
-            prior = model.rollout.rollout_transition(T-1, actions[:-1], prev_state)
-            feat = get_feat(prior)
-            image_pred = torch.cat((((observations[0][0]+0.5)*255).unsqueeze(0).unsqueeze(1)
-                         ,model.observation_decoder(feat).mean))
+            
             prev_state=model.representation.initial_state(1, device=device, dtype=torch.float)
             _, post = model.rollout.rollout_representation(T, embed, actions, prev_state)
 
             feat = get_feat(post)
             post_pred = model.observation_decoder(feat).mean
+
+            prior = model.rollout.rollout_transition(T-t, actions[t-1:-1], prev_state)
+            feat = get_feat(prior)
+            image_pred = torch.cat((post_pred[:t]
+                         ,model.observation_decoder(feat).mean))
         diff=torch.abs(observations[:]-image_pred)
         img_p=np.clip((np.array(image_pred)+0.5)*255,0,255).squeeze(1).transpose((0,2,3,1)).astype(np.uint8)
         img_post=np.clip((np.array(post_pred)+0.5)*255,0,255).squeeze(1).transpose((0,2,3,1)).astype(np.uint8)
@@ -131,9 +135,13 @@ class Evaluator:
         '''
 
 def eval(load_model_path, cuda_idx=None, game="cartpole_balance",itr=10, eval_model=None, 
-        save=True, log_dir=None,rand=True):
+        save=True, log_dir=None,rand=True,T=100, min_cos=0.98,t=5):
     domain, task = game.split('_')
     
+    domain, task = game.split('_',1)
+    if '_' in task:
+        d,task=task.split('_')
+        domain+='_'+d
     params = torch.load(load_model_path) if load_model_path else {}
     agent_state_dict = params.get('agent_state_dict')
     optimizer_state_dict = params.get('optimizer_state_dict')
@@ -149,10 +157,10 @@ def eval(load_model_path, cuda_idx=None, game="cartpole_balance",itr=10, eval_mo
     env=factory_method(name=game)
     agent.initialize(env.spaces)
     agent.to_device(cuda_idx)
-    evaluator=Evaluator(agent, env, cuda_idx=cuda_idx,game=game)
+    evaluator=Evaluator(agent, env, cuda_idx=cuda_idx,game=game,T=T,min_cos=min_cos)
     
     if eval_model is not None:
-        evaluator.eval_model(T=eval_model,rand=rand)
+        evaluator.eval_model(T=eval_model,rand=rand,t=t)
     else:
         for i in tqdm(range(itr)):
             path = None
@@ -178,11 +186,16 @@ if __name__ == "__main__":
     parser.add_argument('--run-ID', help='run identifier (logging)', type=int, default=0)
     parser.add_argument('--load-model-path', help='load model from path', type=str)  # path to params.pkl
     parser.add_argument('--model', help='evaluate model', type=int, default=None)
+    parser.add_argument('--t', help='evaluate model', type=int, default=5)
+    
     parser.add_argument('--itr', help='total iter', type=int,default=10)  # path to params.pkl
 
     parser.add_argument('--rand', help='rand action', type=bool,default=True)  # path to params.pkl
 
     parser.add_argument('--save', help='save', type=bool,default=True)  # path to params.pkl
+    parser.add_argument('--T', type=int, default=100)
+
+    parser.add_argument('--min-cos', type=float, default=0.98)
     args = parser.parse_args()
 
     load_dir = os.path.dirname(args.load_model_path)
@@ -207,8 +220,11 @@ if __name__ == "__main__":
         game=args.game,
         itr=args.itr,
         eval_model=args.model,
+        t=args.t,
         save=args.save,
         log_dir=log_dir,
+        T=args.T,
+        min_cos=args.min_cos,
         rand=args.rand
         )
  
