5776454c82bd4123c49945afc192ce145db44570
kaiqi-ken <jyx11011@gmail.com>
2022-04-03 00:58:07 +0700

    modify

--- dreamer/models/mpc_planner.py
+++ dreamer/models/mpc_planner.py
@@ -38,7 +38,7 @@ class MPC_planner:
         self._dtype=torch.float
 
         if goal_weights is None:
-            goal_weights = torch.cat((0.1*torch.ones(dynamics._stoch_size, dtype=self._dtype),
+            goal_weights = torch.cat((torch.ones(dynamics._stoch_size, dtype=self._dtype),
                                     torch.ones(dynamics._deter_size, dtype=self._dtype)))
         self._goal_weights = goal_weights
         q = torch.cat((
